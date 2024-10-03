b6cc28ed4c992e6c52853ea8671020d78a111dac
markiniorojas <markiniorojas@gmail.com>
2024-10-03 09:58:39 -0500

    actualizacion de carpeta

---
+++ EjerciciosPhp/EjerciciOperaciones/libreria/numeros.php
@@ -0,0 +1,17 @@
+<?php
+
+class Numero {
+  
+  private $valor;
+  
+     public function __construct($valor){
+        $this->valor= $valor;
+     }
+     public function setNumero(){
+       $this ->valor;
+     }
+     public function getNumero(){
+       return $this ->valor;
+     }
+  }
+?>
